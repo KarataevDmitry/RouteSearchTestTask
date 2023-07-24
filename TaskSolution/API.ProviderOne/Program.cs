ed24a153192eb7788c44d37e99317af16965d5ee
Dmitry Karataev <dkarataev1990@gmail.com>
2023-07-24 23:28:24 +0300

    GraphQL Query populated

--- TaskSolution/API.ProviderOne/Program.cs
+++ TaskSolution/API.ProviderOne/Program.cs
@@ -20,7 +20,11 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddScoped<ITravelRouteRepository, TravelRouteRepository>();
 builder.Services.AddScoped<ITravelPointRepository, TravelPointRepository>();
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("SqlServer")));
+builder.Services.AddDbContext<ApplicationDbContext>(
+    options => 
+    options.UseSqlServer(configuration.GetConnectionString("SqlServer")));
+builder.Services.AddGraphQLServer()
+    .AddQueryType<Query>().AddProjections().AddFiltering().AddSorting();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -32,25 +36,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateTime.Now.AddDays(index),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
+app.MapGraphQL("/graphql");
 app.Run();
 
 internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
---
+++ TaskSolution/TaskSolution.DAL/Data/Query.cs
@@ -0,0 +1,23 @@
+﻿using HotChocolate;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskSolution.DAL.Models;
+
+namespace TaskSolution.DAL.Data
+{
+    public class Query
+    {
+        [UseProjection]
+        [UseFiltering]
+        [UseSorting]
+        public IQueryable<TravelPoint> GetTravelPoints([Service]ApplicationDbContext context) => context.TravelPoints;
+        [UseProjection]
+        [UseFiltering]
+        [UseSorting]
+        public IQueryable<TravelRoute> GetTravelRoutes([Service] ApplicationDbContext context) => context.TravelRoutes;
+    }
+}
