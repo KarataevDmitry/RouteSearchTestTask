faa293d554e6e18dcf0ccc9e954c038ccbf976a0
Dmitry Karataev <dkarataev1990@gmail.com>
2023-07-26 21:59:44 +0300

    Strawberry Shake deleted I'll try ZeroQL or another library, 'cause SS give me an errors during client generation

--- TaskSolution/TaskSolution.Clients.Seeder/Program.cs
+++ TaskSolution/TaskSolution.Clients.Seeder/Program.cs
@@ -1,9 +1,8 @@
 
-using HotChocolate;
+
 
 using System.Runtime.CompilerServices;
 
-using TaskSolution.Clients.Seeder;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,8 +10,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddProviderOneClient(StrawberryShake.ExecutionStrategy.CacheFirst)
-    .ConfigureHttpClient(_ => _.BaseAddress = new Uri("http://localhost:5000/graphql"));
+//builder.Services.AddProviderOneClient(StrawberryShake.ExecutionStrategy.CacheFirst)
+    //.ConfigureHttpClient(_ => _.BaseAddress = new Uri("http://localhost:5000/graphql"));
 var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -23,10 +22,17 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-async Task CreateRoute([Service] IProviderOneClient client)
-{
-    var tr = client.GetTravelRoutes;
-}
-
+//async Task CreateRoute([Service] IProviderOneClient client, 
+//                                    string startPointName,
+//                                               string endPointName,
+//                                               DateTime startDateTimeUTC,
+//                                               DateTime arrivalDateTimeUTC,
+//                                               TimeSpan timeToLive,
+//                                               int cost)
+//{
+//    var tr = await client.CreateTravelRoute.ExecuteAsync(startPointName, endPointName, startDateTimeUTC, arrivalDateTimeUTC, timeToLive, cost);
+//}
+//var c = app.Services.GetRequiredService<IProviderOneClient>();
+//await CreateRoute(c, "Alabama", "New York", new DateTime(2023, 12, 5, 3, 15, 15).ToUniversalTime(), new DateTime(2023, 12, 5, 3, 15, 15).ToUniversalTime(), TimeSpan.FromMinutes(15), 999);
 app.Run();
 
